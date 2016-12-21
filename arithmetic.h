71153de1c0c799eb97da15b76f7437fc9e9465e6
Mark Kovalev <restonich@gmail.com>
2016-12-21 13:43:04 +0400

    worst division ever

--- arithmetic.h
+++ arithmetic.h
@@ -5,3 +5,4 @@ int cmp(num_stack *a, num_stack *b);
 num_stack* addit(num_stack *a, num_stack *b);
 num_stack* subst(num_stack *a, num_stack* b);
 num_stack* multi(num_stack *a, num_stack *b);
+num_stack* divis(num_stack *a, num_stack *b);
